341b0e0ffb42c8c32a6db2d01e523dced0e635a8
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-22 00:15:42 +0300

    Refactor before_request function for improved readability and performance

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,8 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth  # Add this line
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -22,6 +22,29 @@ elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
 
 
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+    Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """
+    Forbidden handler.
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.before_request
 def before_request():
     """
@@ -35,27 +58,6 @@ def before_request():
             abort(401)
         if not auth.current_user(request):
             abort(403)
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler
-    """
-    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
