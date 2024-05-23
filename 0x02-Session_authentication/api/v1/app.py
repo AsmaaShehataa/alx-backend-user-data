8f524a8d1d372420d439f1e9814a5cf4db1ccb69
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-24 01:23:09 +0300

    Refactor app.py to remove session authentication
    This commit removes the session authentication functionality from the app.py file. The SessionAuth class and related imports have been removed, as well as the corresponding conditionals for session authentication in the auth module. This change aligns with recent user commits that focused on refactoring the authentication module and improving the logic and consistency of the code.
    
    This commit message follows the established convention of using a prefix to indicate the type of change, in this case, "refactor" for code refactoring.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,7 +5,6 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
-from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -21,14 +20,6 @@ if getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 elif getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
-elif getenv("AUTH_TYPE") == "session_auth":
-    auth = SessionAuth()
-elif getenv('AUTH_TYPE') == 'session_exp_auth':
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-elif getenv('AUTH_TYPE') == 'session_db_auth':
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
